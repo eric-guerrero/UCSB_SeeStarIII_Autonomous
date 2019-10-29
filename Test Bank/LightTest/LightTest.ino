e2becd57298e66a02d7e8e58512ead1d3a2ac3f1
Eric Guerrero <52993762+eric-guerrero@users.noreply.github.com>
2019-10-29 10:29:55 -0700

    Update LightTest.ino

--- Test Bank/LightTest/LightTest.ino
+++ Test Bank/LightTest/LightTest.ino
@@ -2,7 +2,7 @@
 /*
   Turns on SeeStar LED module on for 3 seconds, then off for one second, repeatedly. For testing purposes only.
   This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 License.
-  To view a copy of this license, see http://creativecommons.org/licenses/by-sa/4.0
+  To view a copy of this license, see http://creativecommons.org/licenses/by-sa/4.0.
   
   Modified by Eric Guerrero
  */
